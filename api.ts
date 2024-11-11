07cce5024c2d2656a315bdec116b28387d57f8b4
Pro-jay <projaycobest@gmail.com>
2024-11-11 16:09:38 +0100

    todo app

--- api.ts
+++ api.ts
@@ -3,13 +3,13 @@ import { ITask } from "./types/tasks";
 const baseUrl = "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-  const res = await fetch(`${baseUrl}/tasks`);
+  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
   const todos = await res.json();
   return todos;
 };
 export const addTodo = async (todo: ITask): Promise<ITask> => {
   const res = await fetch(`${baseUrl}/tasks`, {
-    method: "post",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
@@ -17,4 +17,22 @@ export const addTodo = async (todo: ITask): Promise<ITask> => {
   });
   const newTodo = await res.json();
   return newTodo;
+};
+
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  const updatedTodo = await res.json();
+  return updatedTodo;
+};
+
+export const deleteTodo = async (id: string): Promise<void> => {
+  await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+  });
 };
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,15 +1,62 @@
+"use client";
+
 import { ITask } from "@/types/tasks";
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { editTodo } from "@/api";
 
 interface TasksProps {
   task: ITask;
 }
 
 const Task: React.FC<TasksProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setTaskToEdit("");
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
   return (
     <tr key={task.id}>
-      <td>{task.text}</td>
-      <td>Blue</td>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit
+          onClick={() => setOpenModalEdit(true)}
+          cursor="pointer"
+          className="text-blue-600"
+          size={25}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit Task</h3>
+            <div className="modal-action">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full "
+              />
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2 cursor="pointer" className="text-red-600" size={25} />
+      </td>
     </tr>
   );
 };
